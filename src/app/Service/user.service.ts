a4e40d350cdd8a20d977ee02c2a07da40b1a4c53
Karan Bhoi <karanbhoi3087@gmail.com>
2024-07-07 13:32:56 +0530

    code for login and register

--- src/app/Service/user.service.ts
+++ src/app/Service/user.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../Interface/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  baseUrl: String = "https://zonal-beauty-production.up.railway.app/api"
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(): Observable<any> {
+    return this.httpClient.get(`${this.baseUrl}/login`);
+  }
+
+  register(user: User): Observable<any> {
+    return this.httpClient.post(`${this.baseUrl}/register`, user);
+  }
+
+
 }
