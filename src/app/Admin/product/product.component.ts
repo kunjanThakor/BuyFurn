9efb5c950a93fe9f4021897991c34a26d0d4fd33
Karan Bhoi <karanbhoi3087@gmail.com>
2024-07-09 12:14:13 +0530

    remove previous add product code and set to that into AddProductComponent componet

--- src/app/Admin/product/product.component.ts
+++ src/app/Admin/product/product.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { ProductService } from '../../Service/product.service';
 import Swal from 'sweetalert2';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -15,74 +16,6 @@ import Swal from 'sweetalert2';
 
 
 export class ProductComponent {
-
-  product: any = {
-    title: "",
-    description: "",
-    price: "",
-    warranty: "",
-    category: ""
-  };
-  selectedFiles: File[] = [];
-
-  constructor(private productService: ProductService) { }
-
-  onFileSelected(event: any) {
-    const files: FileList = event.target.files;
-    this.selectedFiles = Array.from(files); // Convert FileList to Array
-  }
-
-
-  addProduct() {
-    this.productService.addProduct(this.product, this.selectedFiles)
-      .subscribe(response => {
-        Swal.fire("Product added!");
-        this.resetForm();
-      }, error => {
-        Swal.fire('Error adding product');
-      });
-
-
-
-  }
-
-  resetForm() {
-    const fileInput: HTMLInputElement = document.getElementById('images') as HTMLInputElement;
-    if (fileInput) {
-      fileInput.value = ''; // Clear the value of the file input
-    }
-    // this.selectedFiles = []
-    this.product = {
-      title: '',
-      description: '',
-      price: "",
-      warranty: "",
-      category: '',
-
-    };
-  }
-
-  @ViewChild('addProductModal') addProductModal!: ElementRef;
-
-
-  openAddProductModal() {
-    if (this.addProductModal) {
-      const modal: any = this.addProductModal.nativeElement;
-      modal.classList.add('show');
-      modal.style.display = 'block';
-      modal.setAttribute('aria-modal', 'true');
-    }
-  }
-
-  closeAddProductModal() {
-    if (this.addProductModal) {
-      const modal: any = this.addProductModal.nativeElement;
-      modal.classList.remove('show');
-      modal.style.display = 'none';
-      modal.removeAttribute('aria-modal');
-    }
-  }
-
 
 
 }
