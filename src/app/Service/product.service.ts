f92a5fba264b0408881a0a886b091b345e9f9f27
Karan Bhoi <karanbhoi3087@gmail.com>
2024-07-09 19:57:36 +0530

    remove unnecessary parameter index from forEach funtion

--- src/app/Service/product.service.ts
+++ src/app/Service/product.service.ts
@@ -15,7 +15,7 @@ export class ProductService {
     const formData: FormData = new FormData();
     formData.append('product', JSON.stringify(product));
 
-    images.forEach((image, index) => {
+    images.forEach((image) => {
       formData.append('imgs', image, image.name);
     });
 
@@ -28,5 +28,18 @@ export class ProductService {
 
   getProductById(id: any): Observable<any> {
     return this.httpclient.get(`${this.baseUrl1}/getbyid/${id}`)
+  }
+
+  deleteProductById(id: any): Observable<any> {
+    return this.httpclient.delete(`${this.baseUrl}/deletebyid/${id}`)
+  }
+
+  updateProductById(product: any, images: File[]): Observable<any> {
+    const formData: FormData = new FormData();
+    formData.append('product', JSON.stringify(product));
+
+    images.forEach((image) => {
+      formData.append('imgs', image, image.name);
+    }); return this.httpclient.post(`${this.baseUrl}/updateproduct`, formData)
   }
 }
