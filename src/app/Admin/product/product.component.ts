a91e8794bd8e0012a05dfd15fd0cb7d183a032ff
Karan Bhoi <karanbhoi3087@gmail.com>
2024-07-09 19:53:25 +0530

    now admin can see all products and also add delete and view product functionality

--- src/app/Admin/product/product.component.ts
+++ src/app/Admin/product/product.component.ts
@@ -4,6 +4,8 @@ import { FormsModule, NgModel } from '@angular/forms';
 import { ProductService } from '../../Service/product.service';
 import Swal from 'sweetalert2';
 import { RouterLink } from '@angular/router';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
   selector: 'app-product',
@@ -13,9 +15,47 @@ import { RouterLink } from '@angular/router';
   styleUrl: './product.component.css'
 })
 
+export class ProductComponent implements OnInit {
 
 
-export class ProductComponent {
+  products: any = []
+  loading: boolean = true;
+
+  constructor(private productService: ProductService) { }
+  ngOnInit(): void {
+    this.loadProducts()
+  }
+
+  loadProducts() {
+    this.productService.getAllProducts().subscribe(response => {
+      this.products = response;
+      this.loading = false;
+    },
+      error => {
+        console.log(error);
+        this.loading = false;
+
+      })
+  }
+  deleteProduct(id: any) {
+    this.productService.deleteProductById(id).subscribe(
+      response => {
+        if (response.status === 200) {
+          this.loadProducts();
+          console.log("Product deleted successfully", response);
+        } else {
+          console.log("Product deleted but with unexpected status code", response);
+        }
+      },
+      error => {
+        console.error("Error deleting product", error);
+      }
+    );
+  }
+  editProduct(id: any) {
+    console.log(id);
+
+  }
 
 
 }
